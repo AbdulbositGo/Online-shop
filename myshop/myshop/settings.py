9b9c27ef011831c841b465081fa0da67ffe0ea5a
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-05-21 20:11:24 +0500

    Cart app and Cart session added

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
