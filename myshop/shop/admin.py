ca59e876572c7d042e90ce7fdfa4ed61b4b7b8c0
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-05-21 06:00:41 +0500

    Models added to admin

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.registry(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_dislplay = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'crated', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
