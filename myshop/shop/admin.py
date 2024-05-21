ff2de2da5f9d8815c3a619d07fe5cb6bff1d0f06
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-05-21 19:12:12 +0500

    Template and static files added

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-@admin.registry(Category)
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_dislplay = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
@@ -12,6 +12,6 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class Product(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
-    list_filter = ['available', 'crated', 'updated']
+    list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
