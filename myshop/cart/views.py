c824ca8ee796ddd3f073eb1968061792ec047635
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-05-22 11:29:03 +0500

    Cart session class and cart forms added

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from shop.models import Products
+
