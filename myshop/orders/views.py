e05eec1e557dd4c00d28a45774cae619a7233d4a
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-05-24 10:48:13 -0700

    create order celery added

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
-from .models import Order, OrderItem
+from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
 from cart.cart import Cart
 
 
@@ -19,6 +20,7 @@ def order_create(request):
                     quantity=item['quantity']
                 )
             cart.clear()
+            order_created.delay(order.id)
         return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
