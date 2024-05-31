0276ac03b1dfb06b81472a2b024bfc0a1ad45a02
Abdulbpsit Gofforaliyev <abdulbpsit.go@gmail.com>
2024-05-31 18:12:24 +0500

    Payment templates added

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
-    'payment.app.PaymentConfig',
+    'payment.apps.PaymentConfig',
 ]
 
 MIDDLEWARE = [
